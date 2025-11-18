ca1585bd4ae3401807f647b3c6e5baefdd331858
Gourab Bandyopadhyay <gourabb1.vc@flipkart.com>
2025-11-18 16:56:30 +0530

    fix: updated the rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -25,10 +25,17 @@ const configurePlugins = ({module}) => {
           '@babel/preset-env',
           {
             bugfixes: true,
-            targets: {
-              node: 'current',
-              browsers: '> 1%, last 2 versions',
-            },
+            targets: [
+              // Match browsers supporting "Baseline Widely Available" features:
+              // https://web.dev/baseline
+              'chrome >0 and last 2.5 years',
+              'edge >0 and last 2.5 years',
+              'safari >0 and last 2.5 years',
+              'firefox >0 and last 2.5 years',
+              'and_chr >0 and last 2.5 years',
+              'and_ff >0 and last 2.5 years',
+              'ios >0 and last 2.5 years',
+            ],
           },
         ],
       ],
